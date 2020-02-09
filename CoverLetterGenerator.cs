7d99731c2d20f11636e35b4e70d05cc36049e3fa
Richard Estes <richard.estes48@gmail.com>
2020-02-09 15:28:56 -0800

    first commit

---
+++ CoverLetterGenerator.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Packaging;
+using Google.Maps;
+using Google.Maps.Geocoding;
+
+namespace myWebApp
+{
+    public class CoverLetterGenerator
+    {
+        // private fields
+        private string _companyName;
+        private string _positionTitle;
+        private string _companyNameInput;
+        private string _positionTitleInput;
+        private DateTime _currentDateTime;
+        private string _currentDate;
+        private string _newDocPath;
+        private string _formattedCompanyName;
+        private string _formattedPositionTitle;
+        private string _closestCity;
+        private string _closestCityInput;
+        private AddressObject _companyAddressObj;
+        private string _documentText;
+        private Regex _regexCompanyName;
+        private Regex _regexCurrentDate;
+        private Regex _regexPositionTitle;
+        private Regex _regexCompanyStreet;
+        private Regex _regexCompanyCityState;
+        private Regex _regexCompanyZipCode;
+
+        // class properties
+        public string CompanyName
+        {
+            get => _companyName;
+            set => _companyName = value;
+        }
+        public string PositionTitle
+        {
+            get => _positionTitle;
+            set => _positionTitle = value;
+        }
+        public string CompanyNameInput
+        {
+            get => _companyNameInput;
+            set => _companyNameInput = value;
+        }
+        public string PositionTitleInput
+        {
+            get => _positionTitleInput;
+            set => _positionTitleInput = value;
+        }
+        public DateTime CurrentDateTime
+        {
+            get => _currentDateTime;
+            set => _currentDateTime = value;
+        }
+        public string CurrentDate
+        {
+            get => _currentDate;
+            set => _currentDate = value;
+        }
+        public string NewDocPath
+        {
+            get => _newDocPath;
+            set => _newDocPath = value;
+        }
+        public string FormattedCompanyName
+        {
+            get => _formattedCompanyName;
+            set => _formattedCompanyName = value;
+        }
+        public string FormattedPositionTitle
+        {
+            get => _formattedPositionTitle;
+            set => _formattedPositionTitle = value;
+        }
+        public string ClosestCity
+        {
+            get => _closestCity;
+            set => _closestCity = value;
+        }
+        public string ClosestCityInput
+        {
+            get => _closestCityInput;
+            set => _closestCityInput = value;
+        }
+        public AddressObject CompanyAddressObj
+        {
+            get => _companyAddressObj;
+            set => _companyAddressObj = value;
+        }
+        public string DocumentText
+        {
+            get => _documentText;
+            set => _documentText = value;
+        }
+        public Regex RegexCompanyName
+        {
+            get => _regexCompanyName;
+            set => _regexCompanyName = value;
+        }
+        public Regex RegexCurrentDate
+        {
+            get => _regexCurrentDate;
+            set => _regexCurrentDate = value;
+        }
+        public Regex RegexPositionTitle
+        {
+            get => _regexPositionTitle;
+            set => _regexPositionTitle = value;
+        }
+        public Regex RegexCompanyStreet
+        {
+            get => _regexCompanyStreet;
+            set => _regexCompanyStreet = value;
+        }
+        public Regex RegexCompanyCityState
+        {
+            get => _regexCompanyCityState;
+            set => _regexCompanyCityState = value;
+        }
+        public Regex RegexCompanyZipCode
+        {
+            get => _regexCompanyZipCode;
+            set => _regexCompanyZipCode = value;
+        }
+        // constructor
+        public CoverLetterGenerator()
+        {
+            CompanyAddressObj = new AddressObject();
+        }
+
+
+        public class AddressObject
+        {
+
+            // private fields
+            private string _streetAddress;
+            private string _city;
+            private string _state;
+            private string _cityState;
+            private string _zipCode;
+
+            // class properties
+            public string StreetAddress
+            {
+                get => _streetAddress;
+                set => _streetAddress = value;
+            }
+            public string City
+            {
+                get => _city;
+                set => _city = value;
+            }
+            public string State
+            {
+                get => _state;
+                set => _state = value;
+            }
+            public string CityState
+            {
+                get => _cityState;
+                set => _cityState = value;
+            }
+            public string ZipCode
+            {
+                get => _zipCode;
+                set => _zipCode = value;
+            }
+
+        }
+
+        public void GetCompanyName()
+        {
+            Console.Write("Enter the company name - ");
+            CompanyNameInput = Console.ReadLine();
+            CompanyName = CompanyNameInput;
+            FormattedCompanyName = FormatString(CompanyNameInput);
+            return;
+        }
+        public void GetPositionTitle()
+        {
+            Console.Write("Enter the position title - ");
+            PositionTitleInput = Console.ReadLine();
+            PositionTitle = PositionTitleInput;
+            FormattedPositionTitle = FormatString(PositionTitleInput);
+        }
+        public void GetTime()
+        {
+            CurrentDateTime = DateTime.Today; // As DateTime
+            CurrentDate = CurrentDateTime.ToString("MM/dd/yyyy"); // As String
+        }
+        public string FormatString(string input)
+        {
+            return input.Replace(" ", "");
+        }
+        public void GetNewDocPath()
+        {
+            NewDocPath = "../output/" + FormattedCompanyName + "-" + FormattedPositionTitle + "-CoverLetter.docx";
+        }
+
+        public void SetupRegex()
+        {
+            Regex regexCompanyName = new Regex("COMPANYNAME");
+            Regex regexCurrentDate = new Regex("CURRENTDATE");
+            Regex regexPositionTitle = new Regex("COMPANYPOSITION");
+            //regex is stupid so i have to do this
+            Regex regexCompanyStreet = new Regex("COMPANYLOCATION");
+            Regex regexCompanyCityState = new Regex("COMPANYCITYSTATE");
+            Regex regexCompanyZipCode = new Regex("COMPANYZIPCODE");
+        }
+        public void RegexReplace()
+        {
+            // regex replace
+            DocumentText = RegexCompanyName.Replace(DocumentText, CompanyName);
+            DocumentText = RegexCurrentDate.Replace(DocumentText, CurrentDate);
+            DocumentText = RegexPositionTitle.Replace(DocumentText, PositionTitle);
+            DocumentText = RegexCompanyStreet.Replace(DocumentText, CompanyAddressObj.StreetAddress);
+            DocumentText = RegexCompanyCityState.Replace(DocumentText, CompanyAddressObj.StreetAddress);
+            DocumentText = RegexCompanyZipCode.Replace(DocumentText, CompanyAddressObj.ZipCode);
+        }
+        public string GetClosestCity()
+        {
+            Console.Write("(Optional): Enter the nearest city to " + CompanyName + " - ");
+            ClosestCityInput = Console.ReadLine();
+            ClosestCity = ClosestCityInput;
+            string googleMapsSearchQuery = CompanyName + ClosestCity;
+            return googleMapsSearchQuery;
+        }
+
+        public void GetCompanyAddress()
+        {
+            GoogleSigned.AssignAllServices(new GoogleSigned("AIzaSyCtOR1S6lPWMyK1jE9IiTGMX10-s8z3e9Q"));
+            var request = new GeocodingRequest();
+            request.Address = GetClosestCity();
+            var response = new GeocodingService().GetResponse(request);
+            //The GeocodingService class submits the request to the API web service, and returns the
+            //response strongly typed as a GeocodeResponse object which may contain zero, one or more results.
+
+            //Assuming we received at least one result, let's get some of its properties:
+            if (response.Status == ServiceResponseStatus.Ok && response.Results.Count() > 0)
+            {
+                var result = response.Results.First();
+                string[] addressString = result.FormattedAddress.Split(","); // ex 12345 Street St, City, State Zip
+                string companyStreet = addressString[0]; // street address
+                string companyCity = addressString[1]; // city
+                string companyStateZip = addressString[2]; // state and zipcode
+
+                // remove leading spaces
+                companyStreet = companyStreet.Trim();
+                companyCity = companyCity.Trim();
+                companyStateZip = companyStateZip.Replace(" ", "");
+
+                /// separate companyStateZip into companyState and companyZip WITHOUT REGEX
+                int index = companyStateZip.IndexOfAny(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+                string companyState = companyStateZip.Substring(0, index);
+                int companyZipCode = Int32.Parse(companyStateZip.Substring(index));
+
+                // remove leading space
+                companyState = companyState.Trim();
+
+                // add to AddressObject for access
+                CompanyAddressObj.StreetAddress = companyStreet;
+                CompanyAddressObj.City = companyCity;
+                CompanyAddressObj.State = companyState;
+                CompanyAddressObj.CityState = companyCity + ", " + companyState;
+                CompanyAddressObj.ZipCode = companyZipCode.ToString();
+
+                Console.WriteLine("Full Address: " + result.FormattedAddress); // "1600 Pennsylvania Ave NW, Washington, DC 20500, USA"
+                Console.WriteLine("Street Address: " + CompanyAddressObj.StreetAddress);
+                Console.WriteLine("City: " + CompanyAddressObj.City);
+                Console.WriteLine("State: " + CompanyAddressObj.State);
+                Console.WriteLine("Zip Code: " + CompanyAddressObj.ZipCode);
+
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Unable to geocode.  Status={0} and ErrorMessage={1}", response.Status, response.ErrorMessage);
+            }
+        }
+        public void LogLocation()
+        {
+            return;
+        }
+        public void SearchAndReplace(string document, string outputDocument, string companyName, string positionTitle, string date, AddressObject obj)
+        {
+            // create copy of template document so we don't lose it
+            File.Copy(document, outputDocument);
+
+            // open the file and read contents to var
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(outputDocument, true))
+            {
+                DocumentText = null;
+
+                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    DocumentText = sr.ReadToEnd();
+                }
+
+                // write to new file
+                using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(DocumentText);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace myWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("COVER LETTER GENERATOR");
+            CoverLetterGenerator clg = new CoverLetterGenerator();
+            clg.GetCompanyName();
+        }
+
+        //    static void Main(string[] args)
+        //    {
+        //        Console.WriteLine("COVER LETTER GENERATOR");
+
+        //        string docPath = "/Users/richardestes/Desktop/template.docx";
+        //        string companyNameInput;
+        //        string positionTitleInput;
+        //        AddressObject companyAddressObj = new AddressObject();
+
+        //        // setup date variables
+        //        DateTime today = DateTime.Today; // As DateTime
+        //        string todayAsString = today.ToString("MM/dd/yyyy"); // As String
+
+        //        // get company name
+        //        Console.Write("Enter the company name - ");
+        //        companyNameInput = Console.ReadLine();
+        //        string formattedCompanyName = companyNameInput.Replace(" ", "");
+
+        //        // get position title
+        //        Console.Write("Enter the position title - ");
+        //        positionTitleInput = Console.ReadLine();
+        //        string formattedPositionTitle = positionTitleInput.Replace(" ", "");
+
+        //        // new file name
+        //        // ex - Starbucks-SoftwareEngineer-CoverLetter.docx
+        //        string newDocPath = "/Users/richardestes/Desktop/" + formattedCompanyName + "-" + formattedPositionTitle + "-CoverLetter.docx";
+        //        GetCompanyAddress(companyAddressObj, companyNameInput);
+        //        SearchAndReplace(docPath, newDocPath, companyNameInput, positionTitleInput, todayAsString, companyAddressObj);
+        //        Console.WriteLine("Creating " + companyNameInput + " cover letter...");
+        //    }
+        //    
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
